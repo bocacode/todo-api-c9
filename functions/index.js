545ec46cf904ea2cda1b97ed1d5f0ee5c7779d4a
jSohnData <jihos@me.com>
2023-02-16 00:09:46 -0500

    ADDED delete

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
-import functions from "firebase-functions"
-import express from "express"
+import functions from "firebase-functions";
+import express from "express";
 import cors from "cors"
-import { getAllTasks, addTask, updateTask } from "./src/tasks.js"
+import { getAllTasks, addTask, updateTask, deleteTask } from "./src/tasks.js";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use ( express.json() );
+app.use ( cors() );
 
-app.get('/tasks', getAllTasks)
-app.post('/tasks', addTask)
-app.patch('/tasks/:taskId', updateTask)
+app.get("/tasks", getAllTasks);
+app.post("/tasks", addTask);
+app.patch("/tasks/:taskId", updateTask);
+app.delete("/tasks/:taskId", deleteTask)
 
-export const api = functions.https.onRequest(app)
+export const api = functions.https.onRequest(app);
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,30 +1,54 @@
 import { getFirestoreInstance } from "./utils.js";
 import { FieldValue } from "firebase-admin/firestore";
 
-export async function getAllTasks(req, res) {
-  const db = await getFirestoreInstance();
-  db.collection('tasks').sortBy('createdAt', 'desc').get()
-  .then(collection => {
-      const tasks = collection.docs.map(doc => ({ taskId: doc.id, ...doc.data() }));
-      res.send(tasks);
-    })
-    .catch(err => res.status(500).send({ error: err.message }));
+export function getAllTasks(req, res) {
+    const db = getFirestoreInstance();
+
+    db.collection('tasks')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then( collection => {
+            const tasks = collection.docs.map( element => ({ tasksID: element.id, ...element.data() }));
+            res.send(tasks)
+        })
+        .catch(err => res.status(500).json({ error: err.message}));
 }
+
+
 
 export async function addTask(req, res) {
-  const { task } = req.body;
-  const newTask = { task, createdAt: FieldValue.serverTimestamp() }
-  const db = await getFirestoreInstance();
-  db.collection('tasks').add(newTask)
-    .then(() => getAllTasks(req, res))
-    .catch(err => res.status(500).send({ error: err.message }));
+    const { task } = req.body;
+    const newTask = { task, createdAt: FieldValue.serverTimestamp() }
+    const db = await getFirestoreInstance();
+    
+    db.collection('tasks')
+        .add(newTask)
+        .then( () => getAllTasks(req, res) )
+        .catch(err => res.status(500).send({ error: err.message }));
 }
 
+
+
+export async function deleteTask(req, res) {
+    const { taskId } = req.params;
+    const db = await getFirestoreInstance();
+
+    db.collection('tasks')
+        .doc(taskId)
+        .delete()
+        .then( () => getAllTasks(req, res) )
+        .catch(err => res.status(500).send({ error: err.message }));
+}
+
+
 export async function updateTask(req, res) {
-  const { done } = req.body;
-  const { taskId } = req.params;
-  const db = await getFirestoreInstance();
-  db.collection('tasks').doc(taskId).update({ done })
-    .then(() => getAllTasks(req, res))
-    .catch(err => res.status(500).send({ error: err.message }));
+    const {taskId} = req.params;
+    const {task} = req.body;
+    const db = await getFirestoreInstance();
+
+    db.collection('tasks')
+        .doc(taskId)
+        .update( {task} )
+        .then( () => getAllTasks(req, res) )
+        .catch(err => res.status(500).send({ error: err.message }));
 }
